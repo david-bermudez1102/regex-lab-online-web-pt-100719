5d36a9eea5e3b39d67b7484bd2a6c538de2a4f41
David Bermudez <juan.bermudez1102@outlook.com>
2019-10-10 02:46:06 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,7 +3,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  word.scan(/\A+un+\z+ing/)
+  word.scan(\b+un[a-z]*ing\b)
 end
 
 def words_five_letters_long(text)
